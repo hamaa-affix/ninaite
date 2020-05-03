e74540f7c033468135c170e8924d3920cd1b34d4
hamaa-affix <hamaagive1108@gmail.com>
2020-05-03 10:40:08 +0000

    farmモデル、マイグレーションファイル、コントローラーの作成

---
+++ database/migrations/2020_05_03_062050_create_farms_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFarmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('farms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('representative_name');
+            $table->string('postal_code');
+            $table->string('address1');
+            $table->string('address2');
+            $table->string('address3');
+            $table->string('tel');
+            $table->string('site_uri');
+            $table->string('summary');
+            $table->text('contens');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('farms');
+    }
+}
