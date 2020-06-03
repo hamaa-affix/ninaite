013c516733d79b516fbb0fcc192db03a48c50cd7
hamaa-affix <hamaagive1108@gmail.com>
2020-06-03 15:11:42 +0000

    farms,recruitments.indexのviweを変更

--- resources/views/farms/index.blade.php
+++ resources/views/farms/index.blade.php
@@ -4,23 +4,26 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @foreach($farmDatas as $farmData) 
-            <div class="card">
-                <div class="card-body text-center">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"> <span class="h5">農園名</span><br>{{ $farmData->name }}</li>
-                        <li class="list-group-item"> <span class="h5">郵便番号</span><br>{{ $farmData->address1 }}</li>
-                        <li class="list-group-item"> <span class="h5">都道府県</span><br>{{ $farmData->address2 }}</li>
-                        <li class="list-group-item"> <span class="h5">番地、マンション名</span><br>{{ $farmData->address3 }}</li>
-                        <li class="list-group-item"> <span class="h5">電話番号</span><br>{{ $farmData->tel }}</li>
-                        <li class="list-group-item"> <span class="h5">サイトurl</span><br>{{ $farmData->site_url }}</li>
-                        <li class="list-group-item"> <span class="h5">概要</span><br>{{ $farmData->summary }}</li>
-                        <li class="list-group-item"> <span class="h5">コンテンツ</span><br>{{ $farmData->content }}</li>
-                    </ul>
-                    <a href="{{ route('farms.show', ['farm' => $farmData->id]) }}" class="btn btn-success mt-3">詳細へ</a>
-                </div>
-            </div>
-            @endforeach
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                      <th scope="col">農園名</th>
+                      <th scope="col">概要</th>
+                      <th scope="colspan=2">サイトURL</th>
+                      <th scope="col">詳細画面へ</th>
+                    </tr>
+                </thead>
+                <tbody>
+                  @foreach($farmDatas as $farmData) 
+                    <tr>
+                      <td>{{ $farmData->name }}</td>
+                      <td>{{ $farmData->summary }}</td>
+                      <td>{{ $farmData->site_url }}</td>
+                      <td><a href="{{ route('farms.show', ['farm' => $farmData->id]) }}" class="btn btn-success">詳細へ</a></td>
+                    </tr>
+                  @endforeach
+                 </tbody>
+            </table>
         </div>
     </div>
 </div>
--- resources/views/recruitments/index.blade.php
+++ resources/views/recruitments/index.blade.php
@@ -1,27 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
- <div class="container">
+<div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-             @foreach($recruitments as $recruitment)
-            <div class="card">
-                <div class="card-body text-center">
-                    <div class="form-group text-center">
+        <div class="col-md-8 justify-content-center">
+              @foreach($recruitments as $recruitment)
+                <div class="card border-dark mb-3 text-center justify-content-center" style="max-width: auto;">
+                  <div class="card-header h3">{{ $recruitment->farm()->name}}の案件</div>
+                    <div class="card-body text-dark">
                         <ul class="list-group list-group-flush">
-                            <li class="list-group-item"><span class="h5">農園名</span><br>{{ $recruitment->farm()->name}}</li>
-                            <li class="list-group-item"><span class="h5">案件名</span><br>{{ $recruitment->title }}</li>
-                            <li class="list-group-item"><span class="h5">農園概要</span><br>{{ $recruitment->summary }}</li>
-                            <li class="list-group-item"><span class="h5">仕事内容</span><br>{{ $recruitment->content }}</li>
+                          <li class="list-group-item"><span class="h5">案件名</span><br>{{ $recruitment->title }}</li>
+                          <li class="list-group-item"><span class="h5">農園概要</span><br>{{ $recruitment->summary }}</li>
+                          <li class="list-group-item"><span class="h5">仕事内容</span><br>{{ $recruitment->content }}</li>
                         </ul>
-                        
                         <div class="row justify-content-center">
                             <a href="{{ route('farms.recruitments.show', ['farm' => $recruitment->farm_id, 'recruitment' => $recruitment->id]) }}"  class="btn btn-success mt-3">詳細へ</a>
                         </div>
                     </div>
                 </div>
-            </div>
-             @endforeach
+              @endforeach
         </div>
         <br>
     </div>
