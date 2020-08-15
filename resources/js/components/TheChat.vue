36f6b72023613f5b695c7cb9c6562eaabe4adfc2
hamaa-affix <hamaagive1108@gmail.com>
2020-08-15 00:33:01 +0000

    chat機能の修正

--- resources/js/components/TheChat.vue
+++ resources/js/components/TheChat.vue
@@ -1,20 +1,20 @@
 <template>
-    <div>
+    <!--<div>-->
         <!--if文ロジックをしようしてAuthUserとfromUserで名前とmessageを分けたい。-->
-        <div 
-            v-for="(message, index) in chatMessages"
-            :key="index"
-        >
-           <ul>
-                <li>
-                   <strong>{{ message.user.name }}</strong>
-                   {{ message.body }}
-                </li>
-           </ul>
-        </div>
-        <textarea v-model="postMessage"></textarea>
-        <button @click="sendMessage">送信</button>
-    </div>
+    <!--    <div -->
+    <!--        v-for="(message, index) in chatMessages"-->
+    <!--        :key="index"-->
+    <!--    >-->
+    <!--       <ul>-->
+    <!--            <li>-->
+    <!--               <strong>{{ message.user.name }}</strong>-->
+    <!--               {{ message.body }}-->
+    <!--            </li>-->
+    <!--       </ul>-->
+    <!--    </div>-->
+    <!--    <textarea v-model="postMessage"></textarea>-->
+    <!--    <button @click="sendMessage" :disabled="!textExists">送信</button>-->
+    <!--</div>-->
 </template>
 
 <script>
@@ -29,38 +29,43 @@ export default {
             postMessage: ''
         }
     },
-    //Vueインスタンス作成時にlaravel側のデータをフックして取得
-    created() {
-        this.getMessages();
-        window.Echo.channel("ChatRoomChannel").listen("ChatPusher", (e) => {
-            console.log('receved a message');
-            console.log(e);
-            this.chatMessages.push({
-                body: e.message.body,
-                user: e.user
-            });
-        });
+    // Vueインスタンス作成時にlaravel側のデータをフックして取得
+    // mounted() {
+    //     this.getMessages();
+    //     window.Echo.channel("ChatRoomChannel").listen("ChatPusher", (e) => {
+    //         console.log('receved a message');
+    //         console.log(e);
+    //         this.chatMessages.push({
+    //             body: e.message.body,
+    //             user: e.user
+    //         });
+    //     });
         
-     },
-    methods: {
-        getMessages() {
-            axios.get('/api/chat_messages' )
-            .then((res) => {
-                this.chatMessages = res.data;
-                console.log(res.data);
-            });
-        },
-        sendMessage() {
-            axios.post('/api/chat_messages', { message: this.postMessage })
-            .then((res) => {
-                this.postMessage = '';
-                console.log(res);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-        }
-    }
+    //  },
+    // computed: {
+    //     textExists() {
+    //         return this.chatMessages.length > 0;
+    //     }
+    // },
+    // methods: {
+    //     getMessages() {
+    //         axios.get('/api/chat_messages' )
+    //         .then((res) => {
+    //             this.chatMessages = res.data;
+    //             console.log(res.data);
+    //         });
+    //     },
+    //     sendMessage() {
+    //         axios.post('/api/chat_messages', { message: this.postMessage })
+    //         .then((res) => {
+    //             this.postMessage = '';
+    //             console.log(res);
+    //         })
+    //         .catch(function (error) {
+    //             console.log(error);
+    //         });
+    //     }
+    //}
 }
 </script>
 
