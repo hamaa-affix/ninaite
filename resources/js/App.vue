05afb6edffcde19bac58abe44703f641edf7aac8
hamaa-affix <hamaagive1108@gmail.com>
2020-08-20 04:52:29 +0000

    chatUIの変更

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,20 +1,12 @@
 <template>
     <div>
-        <h1>こんにちはVueです</h1>
         <TheChat></TheChat>
-        <Test></Test>
     </div>
 </template>
 
 <script>
-import TheChat from './components/TheChat';
-import Test from './components/Test';
-
-
     export default {
         components: {
-            TheChat,
-            Test
         }
     }
 </script>
